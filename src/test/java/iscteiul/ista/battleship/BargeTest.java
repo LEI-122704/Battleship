23d08139fb4560f8039a434e6e50fe210ac81843
122704 <pppns@iscte-iul.pt>
2025-10-31 16:21:48 +0000

    Atualização testes barco

--- src/test/java/iscteiul/ista/battleship/BargeTest.java
+++ src/test/java/iscteiul/ista/battleship/BargeTest.java
@@ -2,9 +2,12 @@ package iscteiul.ista.battleship;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Testes de instrução para a classe Barge")
 class BargeTest {
 
     private Barge barge;
@@ -16,24 +19,41 @@ class BargeTest {
         barge = new Barge(Compass.NORTH, startPos);
     }
 
-    @Test
-    @DisplayName("Deve criar Barge corretamente")
-    void testInitialization() {
-        assertNotNull(barge, "Barge não deve ser nula");
-        assertEquals(Compass.NORTH, barge.getBearing(), "Direção inicial incorreta");
+    @Nested
+    @DisplayName("Teste de Inicialização")
+    class InitTests {
+
+        @Test
+        @DisplayName("Barge deve ser criada sem ser nula e com bearing correto")
+        void testInitialization() {
+            assertNotNull(barge, "Barge não deve ser nula");
+            assertEquals(Compass.NORTH, barge.getBearing(), "Direção inicial incorreta");
+        }
+
+        @Test
+        @DisplayName("Posição inicial deve ser atribuída corretamente")
+        void testInitialPositionStored() {
+            assertNotNull(barge.getPositions(), "Lista de posições não deve ser nula");
+            assertEquals(startPos, barge.getPositions().get(0),
+                    "A posição inicial não está correta");
+        }
     }
 
-    @Test
-    @DisplayName("Deve retornar tamanho 1")
-    void testSize() {
-        assertEquals(1, barge.getSize(), "Tamanho do Barge deve ser 1");
-    }
+    @Nested
+    @DisplayName("Teste de Propriedades do Barge")
+    class PropertiesTests {
 
-    @Test
-    @DisplayName("Deve inicializar corretamente a posição do barco")
-    void testPositions() {
-        assertNotNull(barge.getPositions(), "Lista de posições não deve ser nula");
-        assertEquals(1, barge.getPositions().size(), "Barge deve ter exatamente 1 posição");
-        assertEquals(startPos, barge.getPositions().get(0), "A posição inicial não está correta");
+        @Test
+        @DisplayName("Tamanho do Barge deve ser 1")
+        void testSize() {
+            assertEquals(1, barge.getSize(), "Tamanho do Barge deve ser 1");
+        }
+
+        @Test
+        @DisplayName("Lista de posições deve conter exatamente 1 posição")
+        void testPositionsSize() {
+            assertEquals(1, barge.getPositions().size(),
+                    "Barge deve ter exatamente 1 posição");
+        }
     }
 }
